5737e575c3bc0a3894922e525f0c064f08c2edd5
debashiscode <deba.cse.93@hotmail.com>
2025-01-06 00:42:16 +0530

    Add files via upload

---
+++ PythonCourse_DAY6.py
@@ -0,0 +1,88 @@
+# Answer of preorder traversal  Example
+"""
+TRAVERSAL ORDER: A, B, D, C, D, E, F, G, H, and I
+
+"""
+# Answer of inorder traversal  Example
+"""
+TRAVERSAL ORDER: B, D, A, E, H, G, I, F, and C
+
+"""
+
+# Answer of postorder traversal  Example
+"""
+TRAVERSAL ORDER: D, B, H, I, G, F, E, C, and A
+
+"""
+
+"""
+1. Degree of a leaf node is ______.
+(a) 0 (b) 1
+(c) 2 (d) 3
+
+
+2. The depth of root node is ______.
+(a) 0 (b) 1
+(c) 2 (d) 3
+
+3. A binary tree of height h has at least h nodes and
+at most ______ nodes.
+(a) 2h (b) 2h
+(c) 2h+1 (d) 2h – 1
+ 
+ 4. Pre-order traversal is also called ______.
+(a) Depth first (b) Breadth first
+(c) Level order (d) In-order
+
+5. Total number of nodes at the nth level of a binary
+tree can be given as
+(a) 2n (b) 2n
+(c) 2n+1 (d) 2n–1
+"""
+
+#Answer . 1 (a)  2 (a) 3 (d)  4(a)  5(d)
+
+"""
+1. Parent node is also known as the ______ node.
+
+2. Size of a tree is basically the number of ______
+in the tree.
+
+3. The maximum number of nodes at the kth level of
+a binary tree is ______.
+
+4. In a binary tree, every node can have a maximum
+of ______ successors.
+
+5. Nodes at the same level that share the same parent
+are called ______.
+
+6. Two binary trees are said to be copiesif they have
+similar ______ and ______.
+
+7. The height of a binary tree with n nodes is at least
+______ and at most ______.
+
+8. A binary tree T is said to be an extended binary
+tree if ______.
+
+"""
+
+
+
+"""
+Answers
+1. Ascendant 2. Nodes 3. 2k – 1
+4. Two 5. Siblings 6. Structure and contents
+7. n and log2
+(n+1). 8. Each node in the tree has either no child or exactly two children
+"""
+
+
+# Write an algorithm for searching an element in binary tree.
+
+# Write an algorithm for inserting an element into binary tree.
+
+# Write an algorithm for finding the height (or depth) of the binary tree.
+
+# Write an algorithm for deleting an element (assuming data is given) from binary tree.
